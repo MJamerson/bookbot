a5a8df0d9b10d8846b26e88f7aadf59d7a906f9c
MJamerson <MQJamerson@gmail.com>
2025-06-21 20:30:56 +0000

    Split stats functions to own file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def get_word_count(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
