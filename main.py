0bd0739fd3b9f3eb74464703be7923e55a9561b0
MJamerson <MQJamerson@gmail.com>
2025-06-21 23:00:30 +0000

    Add sorted list and report generation

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from stats import get_word_count
 from stats import get_char_count
+from stats import sort_dict_by_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
+def generate_report(filepath, word_count, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for entry in sorted_dict:
+        if str.isalpha(entry["char"]):
+            print(f"{entry["char"]}: {entry["num"]}")
+
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    book_text = get_book_text(filepath)
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
-    print(f"{word_count} words found in the document")
-    print(char_count)
+    sorted_dict = sort_dict_by_count(char_count)
+    generate_report(filepath, word_count, sorted_dict)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def get_char_count(text):
         chars[c] += 1
     
     return chars
+
+def sort_dict_by_count(inp_dict):
+    list_dict = []
+    for key, value in inp_dict.items():
+        list_dict.append({"char": key, "num": value})
+    print(list_dict)
+    list_dict.sort(reverse=True, key=sort_on)
+    print(list_dict)
+    return list_dict
+
+def sort_on(items):
+    return items["num"]
